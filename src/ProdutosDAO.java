438ec386173978bc3b65cc6249d9f0dd06404d15
Camy <camycoder12@gmail.com>
2026-06-05 20:23:25 -0300

    listarProdutosVendidos implementando em ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -74,9 +74,27 @@ public class ProdutosDAO {
         }
         return listagem;
     }
-    
-    
-    
-        
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                vendidos.add(produto);
+            }
+            conn.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+        }
+        return vendidos;
+    }
+
 }
 
